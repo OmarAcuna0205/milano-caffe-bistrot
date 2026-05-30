69520a42a8cc99e601bbe101446b47e259b2dc8a
Omar Acuña <13097@ulsachihuahua.edu.mx>
2026-05-30 12:00:00 -0600

    feat: add About section to Home page and update layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import Hero from "@/components/sections/hero";
 import Menu from "@/components/sections/menu";
 import New from "@/components/sections/new";
+import About from "@/components/sections/about";
 
 export default function Home() {
   return (
     <div className="overflow-x-hidden">
       <Hero />
       <Menu />
-      <New></New>
+      <New />
+      <About />
     </div>
   )
 }
